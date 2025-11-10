a4c360bedda3934bd35ae0c875c8cacdea4708e9
Jakub Kopacki <jkopacki@gmail.com>
2025-11-10 19:49:25 +0100

    Added basic views

--- GUI/pizza-manufacture/src/main.ts
+++ GUI/pizza-manufacture/src/main.ts
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue"
+import router from "./router"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
+import "./assets/custom_style.css"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
