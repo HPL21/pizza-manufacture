0c9fde8cd77bbd4f3c0afde79ce0c91845b35932
Jakub Kopacki <jkopacki@gmail.com>
2025-11-20 19:17:52 +0100

    Added first version of admin panel

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -14,4 +14,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 200
+    }
+  }
 })
